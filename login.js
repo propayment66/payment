fca5d2550652152a330fa4e3a1f1f64275bb77ca
anil bishnoi <digitalindiashop1@gmail.com>
2024-12-06 17:44:23 +0530

    added login.js

---
+++ login.js
@@ -0,0 +1,6 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    localStorage.setItem('username', username);
+    window.location.href = 'index.html';
+});
